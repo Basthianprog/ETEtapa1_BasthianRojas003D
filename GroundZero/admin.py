388136d2085c247b39ee7ec3bf19aa7a15da799e
Basthian Rojas vega <basth.rojasv@gmail.com>
2021-07-07 03:01:47 -0400

    creacion proyecto, clases y conexion a base de datos

---
+++ GroundZero/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Registro, Telefono, Moneda
+# Register your models here.
+
+admin.site.register(Registro)
+admin.site.register(Telefono)
+admin.site.register(Moneda)
